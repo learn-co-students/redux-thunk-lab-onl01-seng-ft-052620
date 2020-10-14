ca6990d6423db78c7d0e33f79e859ab624311e3d
Maloochow <luyao.mly@gmail.com>
2020-10-14 14:47:49 -0400

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,22 @@
-// write your CatList component here
+import React from "react";
+
+// class CatList extends React.Component {
+//   listCats = () => {
+//     return this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.listCats()}
+//       </div>
+//     )
+//   }
+// }
+
+const CatList = ({catPics}) => {
+  return catPics.map((pic, index) => <img src={pic.url} key={index}></img>)
+}
+
+
+export default CatList
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,19 @@
-// write and export your action creator function here
+export const fetchCats = () => {
+    return (dispatch) => {
+        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+            .then(resp => resp.json())
+            .then(json => dispatch({type: 'ADD_CATS', cats: json.images}))
+            .catch(error => console.error(error))
+        dispatch({type: 'LOADING_CATS'})
+    }
+}
+
+// Can I write the following instead??
+
+// function fetchCats(dispatch) {
+//     fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+//             .then(resp => resp.json())
+//             .then(json => dispatch({type: 'ADD_CATS', json}))
+//             .catch(error => console.error(error))
+//         dispatch({type: 'LOADING_CATS'})
+// }
