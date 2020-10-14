a99c99b57d98936c993bb24d385440d12460e57c
ejguzman3988 <eriberto.guzman@outlook.com>
2020-10-14 08:16:05 -0400

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,22 @@
 // write your CatList component here
+import React, { Component } from 'react'
+import { Image } from 'react-bootstrap'
+
+export class CatList extends Component {
+    render() {
+        const renderCatPics = this.props.catPics.map(cat => <img src={cat.url}></img>)
+        
+        if ( this.props.loading ){
+            return(
+                <h1>Loading... *Jeopardy Theme*</h1>
+            )
+        }
+        return (
+            <div>
+                {renderCatPics}
+            </div>
+        )
+    }
+}
+
+export default CatList
